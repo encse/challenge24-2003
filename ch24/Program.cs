66d842edc55841008bfccf146c88ce892fab229d
David Nemeth Csoka <encse@csokavar.hu>
2017-01-01 15:01:46 +0100

    reorg

--- ch24/Program.cs
+++ ch24/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Solvers.Contest03.A;
 using Solvers.Contest03.B;
 using Solvers.Contest03.C;
@@ -15,6 +16,12 @@ namespace Wecomp
 		static void Main()
 		{
 			Lg.dgIlgFromTy = ty => new LgLog4net(ty);
+
+			if (!Directory.Exists("problemset"))
+			{
+				Console.Error.WriteLine("Cannot find `problemset` folder.");	
+				return;
+			}	
 
 			new Ch24Runner<ALettersSolver>("A-{0}.in", "A{0}.out", "A-{0}.refout")
 				.SelectProblems()
